a0460fd42b29152642bb6dbf41ba3423eb48a4f7
tehcGuy <scraptrashscrap@gmail.com>
2021-11-29 15:29:57 +0100

    Almost there

--- src/main/java/com/maksym/dmyterko/tgbotwizard/config/BotConfig.java
+++ src/main/java/com/maksym/dmyterko/tgbotwizard/config/BotConfig.java
@@ -1,13 +1,37 @@
 package com.maksym.dmyterko.tgbotwizard.config;
 /*Class that represents the actual bot bean */
 
+import com.maksym.dmyterko.tgbotwizard.TgBotWizardApplication;
+import com.maksym.dmyterko.tgbotwizard.models.WizardBot;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
 
-@Getter
+
 @Setter
+@Getter
 @Configuration
+@ConfigurationProperties(prefix = "telegrambot")
 public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+
+    @Bean
+    public WizardBot TgBotWizardApplication() {
+        DefaultBotOptions options = ApiContext
+                .getInstance(DefaultBotOptions.class);
+
+        WizardBot mySuperTelegramBot = new WizardBot(options);
+        mySuperTelegramBot.setBotUserName(botUserName);
+        mySuperTelegramBot.setBotToken(botToken);
+        mySuperTelegramBot.setWebHookPath(webHookPath);
+
+        return mySuperTelegramBot;
+    }
 }
--- src/main/java/com/maksym/dmyterko/tgbotwizard/models/WizardBot.java
+++ src/main/java/com/maksym/dmyterko/tgbotwizard/models/WizardBot.java
@@ -2,34 +2,70 @@ package com.maksym.dmyterko.tgbotwizard.models;
 
 /*Class description for the Bean wrapper (BotConfig)*/
 
+import lombok.Setter;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class WizardBot extends TelegramWebhookBot {
-    private String getBotUsername;
-    private String getBotToken;
-    private String getBotPath;
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
 
-
-//    update is the way to get everything from the client (tg id, chat id, message...)
-    @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
+    public void setWebHookPath(String webHookPath) {
+        this.webHookPath = webHookPath;
     }
 
-    @Override
-    public String getBotPath() {
-        return null;
+    public void setBotUserName(String botUserName) {
+        this.botUserName = botUserName;
     }
 
-    @Override
-    public String getBotUsername() {
-        return null;
+    public void setBotToken(String botToken) {
+        this.botToken = botToken;
+    }
+
+    public WizardBot(DefaultBotOptions options) {
+        super(options);
     }
 
     @Override
     public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotPath() {
+        return webHookPath;
+    }
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    //    update is the way to get everything from the client (tg id, chat id, message...)
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        if (update.getMessage() != null && update.getMessage().hasText()) {
+            long chat_id = update.getMessage().getChatId();
+
+
+            try {
+                execute(new SendMessage(chat_id, "Hi " + update.getMessage().getText()));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
         return null;
     }
+
+
+
+
+
+
 }
