61e2461e44797aa61ee41108b83d621967d3daa8
tehcGuy <scraptrashscrap@gmail.com>
2021-12-25 19:30:18 +0100

    Added automatic class-path property files search (ReloadableResourceBundleMessageSource)

---
+++ src/main/java/com/maksym/dmyterko/tgbotwizard/controller/WebHookController.java
@@ -0,0 +1,26 @@
+package com.maksym.dmyterko.tgbotwizard.controller;
+
+import com.maksym.dmyterko.tgbotwizard.models.WizardBot;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@RestController
+public class WebHookController {
+    private final WizardBot telegramBot;
+
+    public WebHookController(WizardBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+
+
+}
