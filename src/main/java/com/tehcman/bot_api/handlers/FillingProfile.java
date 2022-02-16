6b8a1ec36e206bf7370778bb38e836ae52f30609
Max D <53152792+realtehcman@users.noreply.github.com>
2022-02-16 18:47:22 +0100

    added the 1st handler

--- src/main/java/com/tehcman/bot_api/handlers/FillingProfile.java
+++ src/main/java/com/tehcman/bot_api/handlers/FillingProfile.java
@@ -1,6 +1,19 @@
 package com.tehcman.bot_api.handlers;
 
 import com.tehcman.bot_api.InputMessageHandler;
+import com.tehcman.cache.BotState;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 
 public class FillingProfile implements InputMessageHandler {
+
+    @Override
+    public SendMessage handle(Message message) {
+        return null;
+    }
+
+    @Override
+    public BotState getHandlerName() {
+        return null;
+    }
 }
