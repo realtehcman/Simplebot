a70086c8c569f27bdfad6f469a5009a09da516a3
Max D <53152792+realtehcman@users.noreply.github.com>
2022-02-17 15:38:57 +0100

    link

--- src/main/java/com/tehcman/TgBotWizardApplication.java
+++ src/main/java/com/tehcman/TgBotWizardApplication.java
@@ -1,5 +1,5 @@
 package com.tehcman;
-
+//https://www.youtube.com/watch?v=MtKyDKCzENU&t=449s
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
