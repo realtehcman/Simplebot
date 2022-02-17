b499aede618d89752c71ca164322af9b73553e3f
Max D <53152792+realtehcman@users.noreply.github.com>
2022-02-17 09:16:57 +0100

    w pizdu

--- src/main/java/com/tehcman/TgBotWizardApplication.java
+++ src/main/java/com/tehcman/TgBotWizardApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TgBotWizardApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotWizardApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/tehcman/services/MessageService.java
@@ -0,0 +1,8 @@
+package com.tehcman.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageService {
+
+}
