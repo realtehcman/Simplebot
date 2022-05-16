939b2ab1a3735f711d339d01b12ce665aaff75dd
Max D <53152792+realtehcman@users.noreply.github.com>
2022-05-16 15:48:36 +0200

    prettified user.toString()

--- src/main/java/com/tehcman/entities/User.java
+++ src/main/java/com/tehcman/entities/User.java
@@ -40,7 +40,7 @@ public class User {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name   = name;
     }
 
     public String getPhoneNumber() {
@@ -67,16 +67,14 @@ public class User {
         this.age = age;
     }
 
-
+//TODO: move this method to printer class or message class
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", tgUsername='" + tgUsername + '\'' +
-                ", name='" + name + '\'' +
-                ", position=" + position +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", age='" + age + '\'' +
-                '}';
+        return "Your data:" +
+                "\nid=" + id +
+                ",\n tgUsername='" + tgUsername + '\'' +
+                ",\n name='" + name + '\'' +
+                ",\n phoneNumber='" + phoneNumber + '\'' +
+                ",\n age='" + age + '\'';
     }
 }
