230c05df291818b7cb75038f671168ec52270526
Max D <53152792+realtehcman@users.noreply.github.com>
2022-05-17 08:51:25 +0200

    applied DRY principle; about to implement method

--- src/main/java/com/tehcman/services/BuildMessageService.java
+++ src/main/java/com/tehcman/services/BuildMessageService.java
@@ -6,12 +6,7 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
 @Service
 public class BuildMessageService {
